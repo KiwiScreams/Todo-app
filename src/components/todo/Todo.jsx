e9d9fdb14b3c5a4b21479625b262c5e7a1374a98
KiwiScreams <screamskiwi3@gmail.com>
2024-07-25 23:22:12 +0400

    functional

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,43 +1,56 @@
 import "./Todo.css"
 import { useState } from "react";
-function Todo()
-{
+function Todo() {
     const [newTodo, createNewTodo] = useState("");
     const [todos, setTodo] = useState([]);
-    function handleSubmit(e)
-    {
+    function handleSubmit(e) {
         e.preventDefault()
         setTodo(currentTodo => {
-          return [...currentTodo, { id: crypto.randomUUID(), title: newTodo, completed: false }]
+            return [...currentTodo, { id: crypto.randomUUID(), title: newTodo, completed: false }]
         })
         createNewTodo("")
     }
     function toggleTodo(id, completed) {
         setTodo(currentTodo => {
-          return currentTodo.map(todo => {
-            if (todo.id === id) {
-              return { ...todo, completed }
+            return currentTodo.map(todo => {
+                if (todo.id === id) {
+                    return { ...todo, completed }
+                }
+                return todo
             }
-            return todo
-          }
-          )
+            )
         })
-      }
-      function deleteTodo(id) {
+    }
+    function deleteTodo(id) {
         setTodo(currentTodo => {
-          return currentTodo.filter(todo => todo.id !== id)
+            return currentTodo.filter(todo => todo.id !== id)
         })
-      }
+    }
     return (
         <>
-            <form className="todo-container" onSubmit={handleSubmit} 
-            value={newTodo}
-              onChange={e => createNewTodo(e.target.value)}>
-                <div className="input-container">
-                    <div className="round"></div>
-                    <input type="text" name="todo-input" id="todo-input" placeholder="Create a new todo…" />
-                </div>
-            </form>
+            <section className="todo-about">
+                <form className="todo-container" onSubmit={handleSubmit}
+                    value={newTodo}
+                    onChange={e => createNewTodo(e.target.value)}>
+                    <div className="input-container">
+                        <button className="round"></button>
+                        <input type="text" name="todo-input" id="todo-input" placeholder="Create a new todo…" />
+                    </div>
+                </form>
+                <ul className="todo-section">
+                    {todos.length === 0 && "No ToDos"}
+                    {todos.map(todo => {
+                        return <li key={todo.id}>
+                            <label>
+                                <input type="checkbox" checked={todo.completed} onChange={e => toggleTask(todo.id, e.target.checked)} />
+                                {todo.title}
+                            </label>
+                            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                        </li>
+                    }
+                    )}
+                </ul>
+            </section>
         </>
     )
 }
